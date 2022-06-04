25f1df6d0c34507c53a1e75717c4c5b103fa5a05
sam <sammidev4@gmail.com>
2022-06-04 12:18:20 +0700

    create sample test

---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	var tests = []struct {
+		a, b, want int
+	}{
+		{1, 2, 3},
+		{2, 3, 5},
+		{3, 4, 7},
+		{4, 5, 9},
+	}
+
+	for _, test := range tests {
+		if got := Add(test.a, test.b); got != test.want {
+			t.Errorf("Add(%d, %d) = %d, want %d", test.a, test.b, got, test.want)
+		}
+	}
+}
